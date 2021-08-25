ceb958b680cea0a930e54185e0847aa3ca34b72a
4drian3d <adrianed2004@gmail.com>
2021-08-25 13:57:08 -0500

    Improved plugin command

--- src/main/java/com/aang23/tempserv/CommandTempServ.java
+++ src/main/java/com/aang23/tempserv/CommandTempServ.java
@@ -6,10 +6,48 @@ import com.velocitypowered.api.command.CommandSource;
 import com.velocitypowered.api.proxy.server.ServerInfo;
 
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
+import static net.kyori.adventure.text.event.ClickEvent.suggestCommand;
+import static net.kyori.adventure.text.event.HoverEvent.showText;
+
+import static net.kyori.adventure.text.Component.text;
+import static net.kyori.adventure.text.Component.space;
+import static net.kyori.adventure.text.format.NamedTextColor.GREEN;
+import static net.kyori.adventure.text.format.NamedTextColor.YELLOW;
+import static net.kyori.adventure.text.format.NamedTextColor.AQUA;
+import static net.kyori.adventure.text.format.NamedTextColor.RED;
 
 public class CommandTempServ implements SimpleCommand {
-
+    static final Component usage = text()
+        .append(text()
+            .append(text("Usage:", RED))
+        )
+        .append(space())
+        .append(text()
+            .append(text("/tempserv add [name] [adress] [port]", AQUA))
+            .clickEvent(suggestCommand("tempserv add"))
+            .hoverEvent(showText(text("Click to execute", YELLOW)))
+        )
+        .append(space())
+        .append(text()
+            .append(text("or", RED))
+        )
+        .append(space())
+        .append(text()
+            .append(text("/tempserv del [name]", AQUA))
+            .clickEvent(suggestCommand("tempserv del"))
+            .hoverEvent(showText(text("Click to execute", YELLOW)))
+        )
+        .append(space())
+        .append(text()
+            .append(text("or", RED))
+        )
+        .append(space())
+        .append(text()
+            .append(text("/tempserv list", AQUA))
+            .clickEvent(suggestCommand("tempserv list"))
+            .hoverEvent(showText(text("Click to execute", YELLOW)))
+        )
+        .build();
     @Override
     public void execute(final Invocation invocation) {
         CommandSource source = invocation.source();
@@ -23,27 +61,22 @@ public class CommandTempServ implements SimpleCommand {
                 TempServ.server.registerServer(toRegister);
 
                 source.sendMessage(
-                        Component.text("Registered server " + 
-                            args[1] + " with adress " + 
+                        text("Registered server " + 
+                            args[1] + " with address " + 
                             args[2] + ":" + 
                             args[3], 
-                        NamedTextColor.GREEN));
+                            GREEN));
             } else if (args[0].equals("del") && args.length == 2) {
                 TempServ.server.unregisterServer(TempServ.registeredServers.get(args[1]));
-                source.sendMessage(Component.text("Deleted server " + 
-                    args[1], 
-                    NamedTextColor.GREEN));
+                source.sendMessage(text("Deleted server " + args[1], GREEN));
             } else if (args[0].equals("list")) {
                 for (ServerInfo current : TempServ.registeredServers.values()) {
                     source.sendMessage(
-                            Component.text("Server " + current.getName() + " : " + current.getAddress().toString(), 
-                                NamedTextColor.YELLOW));
+                            text("Server " + current.getName() + " : " + current.getAddress().toString(), YELLOW));
                 }
             } else
-                source.sendMessage(Component.text("Usage : /tempserv add [name] [adress] [port] or /tempserv del [name] or /tempserv list", 
-                    NamedTextColor.RED));
+                source.sendMessage(usage);
         } else
-            source.sendMessage(Component.text("Usage : /tempserv add [name] [adress] [port] or /tempserv del [name] or /tempserv list", 
-                NamedTextColor.RED));
+            source.sendMessage(usage);
     }
 }
